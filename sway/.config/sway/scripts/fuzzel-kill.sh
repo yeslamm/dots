e6ad9708ca095699834ab2a3322682bd16e3b337
yousef <yousef.eslam1117@email.com>
2026-03-25 13:19:38 +0200

    added fuzzel-kill script, removed force kill line from config

---
+++ sway/.config/sway/scripts/fuzzel-kill.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Define processes that should NEVER show up in the kill menu
+PROTECTED="sway|waybar|pipewire|wireplumber|dbus|systemd|fuzzel|bash|ssh|polkit"
+
+# 1. Get user processes (quoted "$USER" to fix SC2086)
+# 2. Filter OUT protected processes using awk (fixes SC2009)
+# 3. Sort and remove duplicates
+# 4. Pipe to Fuzzel
+SELECTED_APP=$(ps -u "$USER" -o comm= | awk -v pat="^(${PROTECTED})$" '$0 !~ pat' | sort -u | fuzzel --dmenu --prompt="KILL: ")
+
+if [ -n "$SELECTED_APP" ]; then
+    pkill -9 -x "$SELECTED_APP"
+    notify-send -t 2000 "Process Killed" "Terminated: $SELECTED_APP"
+fi
