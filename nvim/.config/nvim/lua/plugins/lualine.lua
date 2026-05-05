22645223c786fee3eea0cdd85b7b5b573e1f23c0
yousef <yousef.eslam1117@email.com>
2026-05-05 22:11:09 +0300

    style(nvim): moved vague into a united colorscheme.lua and added kanagawa to it

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -55,7 +55,7 @@ return {
             require('lualine').setup {
                 options = {
                     icons_enabled = true,
-                    theme = 'vague',
+                    theme = 'auto',
                     section_separators = { left = '', right = '' },
                     component_separators = { left = '', right = '' },
                     always_divide_middle = true,
