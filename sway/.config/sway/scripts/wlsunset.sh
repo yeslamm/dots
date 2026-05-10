e2d255db42ebf17ce2f3ab1a4a44594629c809e5
yousef <yousef.eslam1117@email.com>
2026-05-10 22:07:50 +0300

    chore(wlsunset.sh): concatenated the notify-send strings together

--- sway/.config/sway/scripts/wlsunset.sh
+++ sway/.config/sway/scripts/wlsunset.sh
@@ -11,11 +11,11 @@ DAY_TEMP="6500"
 
 if pgrep -x wlsunset >/dev/null; then
     pkill -x wlsunset
-    notify-send "Blue Light Filter" "Disabled (Night color off)" \
+    notify-send "Blue Light Filter: OFF" \
         -t 1500 -h string:x-canonical-private-synchronous:gamma
 else
     # Run in background to let the script finish
     wlsunset -l "$LAT" -L "$LON" -t "$NIGHT_TEMP" -T "$DAY_TEMP" &
-    notify-send "Blue Light Filter" "Enabled (Night color on)" \
+    notify-send "Blue Light Filter: OFF" \
         -t 1500 -h string:x-canonical-private-synchronous:gamma
 fi
