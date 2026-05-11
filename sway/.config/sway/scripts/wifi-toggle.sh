2af0908731d4249c110e54a588467b1949a48ed6
yousef <yousef.eslam1117@email.com>
2026-05-11 15:48:26 +0300

    feat: replace nm-applet and blueman with impala and bluetui

--- sway/.config/sway/scripts/wifi-toggle.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-STATE=$(nmcli -t -f WIFI radio)
-if [ "$STATE" = "enabled" ]; then
-    nmcli radio wifi off
-    notify-send -t 1500 -h string:x-canonical-private-synchronous:wifi "WiFi Disabled"
-else
-    nmcli radio wifi on
-    notify-send -t 1500 -h string:x-canonical-private-synchronous:wifi "WiFi Enabled"
-fi
