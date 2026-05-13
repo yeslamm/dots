a09160c26eed3d683129286134edb50744d8b058
yousef <yousef.eslam1117@email.com>
2026-05-13 23:23:19 +0300

    feat(nvim): add highlight-colors & integrate with blink

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -1,6 +1,9 @@
 return {
     'saghen/blink.cmp',
-    dependencies = { 'rafamadriz/friendly-snippets' },
+    dependencies = {
+        'rafamadriz/friendly-snippets',
+        'brenoprata10/nvim-highlight-colors',
+    },
     version = '1.*',
     opts = {
         keymap = {
@@ -25,6 +28,35 @@ return {
                         { 'kind_icon' },
                         { 'label', 'label_description', gap = 1 },
                         { 'kind' },
+                    },
+                    components = {
+                        -- customize the drawing of kind icons
+                        kind_icon = {
+                            text = function(ctx)
+                                -- default kind icon
+                                local icon = ctx.kind_icon
+                                -- if LSP source, check for color derived from documentation
+                                if ctx.item.source_name == 'LSP' then
+                                    local color_item = require('nvim-highlight-colors').format(ctx.item.documentation, { kind = ctx.kind })
+                                    if color_item and color_item.abbr ~= '' then
+                                        icon = color_item.abbr
+                                    end
+                                end
+                                return icon .. ctx.icon_gap
+                            end,
+                            highlight = function(ctx)
+                                -- default highlight group
+                                local highlight = 'BlinkCmpKind' .. ctx.kind
+                                -- if LSP source, check for color derived from documentation
+                                if ctx.item.source_name == 'LSP' then
+                                    local color_item = require('nvim-highlight-colors').format(ctx.item.documentation, { kind = ctx.kind })
+                                    if color_item and color_item.abbr_hl_group then
+                                        highlight = color_item.abbr_hl_group
+                                    end
+                                end
+                                return highlight
+                            end,
+                        },
                     },
                 },
             },
