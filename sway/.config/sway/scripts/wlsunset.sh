e61821495cf11b4c473d2ea0628fff8891963385
yousef <yousef.eslam1117@email.com>
2026-05-17 02:52:23 +0300

    feat: sway/waybar wifi & bluetooth binds, capitalizing 'on|off' across the config

--- sway/.config/sway/scripts/wlsunset.sh
+++ sway/.config/sway/scripts/wlsunset.sh
@@ -11,11 +11,11 @@ DAY_TEMP="6500"
 
 if pgrep -x wlsunset >/dev/null; then
     pkill -x wlsunset
-    notify-send "Blue Light Filter: off" \
+    notify-send "Blue Light Filter: OFF" \
         -t 1500 -h string:x-canonical-private-synchronous:gamma
 else
     # Run in background to let the script finish
     wlsunset -l "$LAT" -L "$LON" -t "$NIGHT_TEMP" -T "$DAY_TEMP" &
-    notify-send "Blue Light Filter: on" \
+    notify-send "Blue Light Filter: ON" \
         -t 1500 -h string:x-canonical-private-synchronous:gamma
 fi
