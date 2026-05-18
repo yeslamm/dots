155cea53b868d8795a0143373cdc826d1a766706
yousef <yousef.eslam1117@email.com>
2026-05-19 01:51:30 +0300

    fix(sway): hardening idle gatekeeper regex

--- sway/.config/sway/scripts/check_procs.sh
+++ sway/.config/sway/scripts/check_procs.sh
@@ -2,8 +2,7 @@
 PROCS_FILE="$HOME/.config/sway/idle_procs"
 
 [[ -f "$PROCS_FILE" ]] || exit 1
-PATS=$(grep -vE '^#|^$' "$PROCS_FILE" | tr '\n' '|' | sed 's/|$//')
+PATS=$(grep -vE '^#|^$' "$PROCS_FILE" | xargs | tr ' ' '|')
 [[ -n "$PATS" ]] || exit 1
 
-# Exits with 0 (Success) if yay, pacman, yt-dlp, etc., are actually running
 pgrep -i "^($PATS)$" >/dev/null
