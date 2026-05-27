52ff32de644e7130c4925e9794613d8fa5d13228
yousef <yousef.eslam1117@email.com>
2026-05-28 01:08:19 +0300

    style(nvim): install nightfox engine and set colorscheme to carbonfox

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,44 +1,36 @@
 return {
     {
+        'EdenEast/nightfox.nvim',
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require('nightfox').setup {
+                options = {
+                    transparent = false, -- Disable setting background
+                    terminal_colors = true, -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
+                },
+            }
+            vim.cmd 'colorscheme carbonfox'
+        end,
+    },
+    {
         'vague-theme/vague.nvim',
-        lazy = false, -- make sure we load this during startup if it is your main colorscheme
-        priority = 1000, -- make sure to load this before all the other plugins
+        lazy = true,
         config = function()
             require('vague').setup {
-                -- optional configuration here
+                transparent = false,
             }
-            vim.cmd 'colorscheme vague'
-            vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
-            vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'none' })
+            -- vim.cmd 'colorscheme vague'
         end,
     },
     {
         'rebelot/kanagawa.nvim',
-        lazy = false,
+        lazy = true,
         config = function()
             require('kanagawa').setup {
-                compile = false, -- enable compiling the colorscheme
-                undercurl = true, -- enable undercurls
-                commentStyle = { italic = true },
-                functionStyle = {},
-                keywordStyle = { italic = true },
-                statementStyle = { bold = true },
-                typeStyle = {},
-                transparent = false, -- do not set background color
-                dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+                transparent = false,
                 terminalColors = true, -- define vim.g.terminal_color_{0,17}
-                colors = { -- add/modify theme and palette colors
-                    palette = {},
-                    theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
-                },
-                overrides = function() -- add/modify highlights
-                    return {}
-                end,
                 theme = 'dragon', -- Load "wave" theme
-                background = { -- map the value of 'background' option to a theme
-                    dark = 'dragon', -- try "dragon" !
-                    light = 'lotus',
-                },
             }
             -- vim.cmd 'colorscheme kanagawa'
         end,
@@ -46,5 +38,6 @@ return {
     {
         'rose-pine/neovim',
         name = 'rose-pine',
+        lazy = true,
     },
 }
