dc34c8291d9bf9ff100f8c106adf178e3c4f525e
yousef <yousef.eslam1117@email.com>
2026-06-04 03:44:49 +0300

    feat(sway): add cliphist-fzf utility for interactive clipboard history

---
+++ sway/.config/sway/scripts/cliphist-fzf.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+case "$1" in
+preview)
+    row="$2"
+    if echo "$row" | grep -vqP '^\d+\t\[\[ binary data .* \]\]'; then
+        echo "$row" | cliphist decode
+    else
+        echo "$row" | cliphist decode | chafa -f sixel -s "${FZF_PREVIEW_COLUMNS}x${FZF_PREVIEW_LINES}"
+    fi
+    ;;
+
+*)
+    id="$(cliphist list | fzf \
+        --delimiter '\t' \
+        --with-nth 2.. \
+        --preview-window=down:50% \
+        --bind "ctrl-d:preview-half-page-down,ctrl-u:preview-half-page-up" \
+        --preview "$(realpath "$0") preview {}")"
+
+    test -z "$id" && exit
+
+    cliphist decode <<<"$id" | wl-copy
+    ;;
+
+esac
