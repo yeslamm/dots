578c89d5d7b7bde3d141feb634b39e274d609d4a
yousef <yousef.eslam1117@email.com>
2026-06-07 06:47:36 +0300

    refactor(cliphist): migrate database path to ephemeral XDG_RUNTIME_DIR

--- sway/.config/sway/scripts/cliphist-fzf.sh
+++ sway/.config/sway/scripts/cliphist-fzf.sh
@@ -1,26 +1,29 @@
 #!/usr/bin/env bash
 
+DB_PATH="${XDG_RUNTIME_DIR:-/run/user/$(id -u)}/cliphist.db"
+CH_CMD="cliphist -db-path $DB_PATH"
+
 case "$1" in
 preview)
     row="$2"
     if echo "$row" | grep -vqP '^\d+\t\[\[ binary data .* \]\]'; then
-        echo "$row" | cliphist decode
+        echo "$row" | $CH_CMD decode
     else
-        echo "$row" | cliphist decode | chafa -f sixel -s "${FZF_PREVIEW_COLUMNS}x${FZF_PREVIEW_LINES}"
+        echo "$row" | $CH_CMD decode | chafa -f sixel -s "${FZF_PREVIEW_COLUMNS}x${FZF_PREVIEW_LINES}"
     fi
     ;;
 
 *)
-    id="$(cliphist -preview-width 1000 list | fzf \
+    id="$($CH_CMD -preview-width 1000 list | fzf \
         --delimiter '\t' \
         --with-nth 2.. \
-        --preview-window=down:50%,wrap \
+        --preview-window=right:55%,wrap \
         --bind "ctrl-d:preview-half-page-down,ctrl-u:preview-half-page-up" \
         --preview "$(realpath "$0") preview {}")"
 
     test -z "$id" && exit
 
-    cliphist decode <<<"$id" | wl-copy
+    $CH_CMD decode <<<"$id" | wl-copy
     ;;
 
 esac
