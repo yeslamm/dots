4713e243abb2248d8fca50f757e542486dfe5aa9
yousef <yousef.eslam1117@email.com>
2026-06-13 05:37:31 +0300

    feat(sway): introduce hardware presentation tearing toggle script

---
+++ sway/.config/sway/scripts/vsync_toggle.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+RUNTIME_DIR="${XDG_RUNTIME_DIR:-/run/user/$(id -u)}"
+STATE_FILE="$RUNTIME_DIR/vsync-flag"
+
+if [ ! -f "$STATE_FILE" ]; then
+    CURRENT_STATE="on"
+else
+    CURRENT_STATE=$(cat "$STATE_FILE")
+fi
+
+if [ "$CURRENT_STATE" = "on" ]; then
+    swaymsg "output * allow_tearing yes"
+    swaymsg "output * max_render_time off"
+
+    echo "off" >"$STATE_FILE"
+
+    notify-send -t 2000 -h string:x-canonical-private-synchronous:vsync "Vsync: OFF"
+else
+    swaymsg "output * allow_tearing no"
+    swaymsg "output * max_render_time 3"
+
+    echo "on" >"$STATE_FILE"
+
+    notify-send -t 2000 -h string:x-canonical-private-synchronous:vsync "Vsync: ON"
+fi
