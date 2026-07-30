d7d76d7f6b8b7e30417177ec239d9d13f72812ba
yousef <yousef.eslam1117@email.com>
2026-07-31 02:55:25 +0300

    refactor(swayimg): standardize Lua style and enhance gallery mode bindings

--- apps/.config/swayimg/init.lua
+++ apps/.config/swayimg/init.lua
@@ -1,97 +1,106 @@
 ---@diagnostic disable: undefined-global
 
 -- =============================================================================
--- SWAYIMG v5.0+ LUA CONFIGURATION (~/.config/swayimg/init.lua)
+-- 1. GENERAL & VISUAL SETTINGS
 -- =============================================================================
-
--- --- 1. GENERAL & VISUAL SETTINGS ---
-swayimg.set_mode("viewer")
+swayimg.set_mode 'viewer'
 swayimg.enable_decoration(false)
 swayimg.enable_antialiasing(true)
 
 swayimg.viewer.set_window_background(0xff000000)
 swayimg.viewer.set_image_chessboard(10, 0xff222222, 0xff444444)
-swayimg.viewer.set_default_scale("optimal")
+swayimg.viewer.set_default_scale 'optimal'
 
--- Hide info text on startup
 swayimg.on_initialized(function()
-	swayimg.text.hide()
+    swayimg.text.hide()
 end)
 
--- --- 2. FILE NAVIGATION (Explicitly mapped for stability) ---
-swayimg.viewer.on_key("Space", function()
-	swayimg.viewer.switch_image("next")
+-- =============================================================================
+-- 2. FILE NAVIGATION
+-- =============================================================================
+swayimg.viewer.on_key('Space', function()
+    swayimg.viewer.switch_image 'next'
 end)
 
-swayimg.viewer.on_key("BackSpace", function()
-	swayimg.viewer.switch_image("prev")
+swayimg.viewer.on_key('BackSpace', function()
+    swayimg.viewer.switch_image 'prev'
 end)
 
--- --- 3. VIM KEYBINDINGS (PANNING) ---
+-- =============================================================================
+-- 3. VIM PANNING & VIEWER CONTROLS
+-- =============================================================================
 local pan_speed = 50
 
-swayimg.viewer.on_key("h", function()
-	local pos = swayimg.viewer.get_position()
-	swayimg.viewer.set_abs_position(pos.x + pan_speed, pos.y)
+swayimg.viewer.on_key('h', function()
+    local pos = swayimg.viewer.get_position()
+    swayimg.viewer.set_abs_position(pos.x + pan_speed, pos.y)
 end)
 
-swayimg.viewer.on_key("l", function()
-	local pos = swayimg.viewer.get_position()
-	swayimg.viewer.set_abs_position(pos.x - pan_speed, pos.y)
+swayimg.viewer.on_key('l', function()
+    local pos = swayimg.viewer.get_position()
+    swayimg.viewer.set_abs_position(pos.x - pan_speed, pos.y)
 end)
 
-swayimg.viewer.on_key("j", function()
-	local pos = swayimg.viewer.get_position()
-	swayimg.viewer.set_abs_position(pos.x, pos.y - pan_speed)
+swayimg.viewer.on_key('j', function()
+    local pos = swayimg.viewer.get_position()
+    swayimg.viewer.set_abs_position(pos.x, pos.y - pan_speed)
 end)
 
-swayimg.viewer.on_key("k", function()
-	local pos = swayimg.viewer.get_position()
-	swayimg.viewer.set_abs_position(pos.x, pos.y + pan_speed)
+swayimg.viewer.on_key('k', function()
+    local pos = swayimg.viewer.get_position()
+    swayimg.viewer.set_abs_position(pos.x, pos.y + pan_speed)
 end)
 
--- --- 4. ZOOM & INFO TOGGLE ---
--- z: Reset to optimal fit and center
-swayimg.viewer.on_key("z", function()
-	swayimg.viewer.set_fix_scale("optimal")
-	swayimg.viewer.set_fix_position("center")
+-- Reset scale & position (z)
+swayimg.viewer.on_key('z', function()
+    swayimg.viewer.set_fix_scale 'optimal'
+    swayimg.viewer.set_fix_position 'center'
 end)
 
--- i: Toggle info text
-swayimg.viewer.on_key("i", function()
-	if swayimg.text.visible() then
-		swayimg.text.hide()
-	else
-		swayimg.text.show()
-	end
+-- Toggle text info layer (i)
+swayimg.viewer.on_key('i', function()
+    if swayimg.text.visible() then
+        swayimg.text.hide()
+    else
+        swayimg.text.show()
+    end
 end)
 
--- --- 5. GALLERY & SYSTEM ---
-swayimg.viewer.on_key("g", function()
-	swayimg.set_mode("gallery")
+-- =============================================================================
+-- 4. GALLERY MODE & NAVIGATION
+-- =============================================================================
+swayimg.viewer.on_key('g', function()
+    swayimg.set_mode 'gallery'
 end)
-swayimg.gallery.on_key("Return", function()
-	swayimg.set_mode("viewer")
+swayimg.gallery.on_key('g', function()
+    swayimg.set_mode 'viewer'
 end)
-
--- Gallery Vim Nav
-swayimg.gallery.on_key("h", function()
-	swayimg.gallery.switch_image("left")
+swayimg.gallery.on_key('Return', function()
+    swayimg.set_mode 'viewer'
 end)
-swayimg.gallery.on_key("l", function()
-	swayimg.gallery.switch_image("right")
-end)
-swayimg.gallery.on_key("j", function()
-	swayimg.gallery.switch_image("down")
-end)
-swayimg.gallery.on_key("k", function()
-	swayimg.gallery.switch_image("up")
+swayimg.gallery.on_key('Escape', function()
+    swayimg.set_mode 'viewer'
 end)
 
--- Quit
-swayimg.viewer.on_key("q", function()
-	swayimg.exit()
+swayimg.gallery.on_key('h', function()
+    swayimg.gallery.switch_image 'left'
 end)
-swayimg.gallery.on_key("q", function()
-	swayimg.exit()
+swayimg.gallery.on_key('l', function()
+    swayimg.gallery.switch_image 'right'
+end)
+swayimg.gallery.on_key('j', function()
+    swayimg.gallery.switch_image 'down'
+end)
+swayimg.gallery.on_key('k', function()
+    swayimg.gallery.switch_image 'up'
+end)
+
+-- =============================================================================
+-- 5. EXIT
+-- =============================================================================
+swayimg.viewer.on_key('q', function()
+    swayimg.exit()
+end)
+swayimg.gallery.on_key('q', function()
+    swayimg.exit()
 end)
